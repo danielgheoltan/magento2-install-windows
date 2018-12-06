59010b30a0a934e04ca2ae11b87ef58b510d37ff
Daniel Gheolţan <daniel.gheoltan@mindmagnetsoftware.com>
2018-12-06 21:44:27 +0200

    Updates

--- config.bat
+++ config.bat
@@ -1,12 +1,12 @@
-set PROJECT_PATH=D:/Magento/ce/v2.2.x
-set PROJECT_HOST=magento-ce-2-2-x.local
+set PROJECT_PATH=D:/Magento/ce/v2.3.0
+set PROJECT_HOST=magento-ce-2-3-0.local
 
 set MYSQL_HOST=localhost
 set MYSQL_USERNAME=root
 set MYSQL_PASSWORD=root
-set MYSQL_DATABASE=magento-ce-2-2-x
+set MYSQL_DATABASE=magento-ce-2-3-0
 
-set MAGENTO_BASE_URL=http://magento-ce-2-2-x.local
+set MAGENTO_BASE_URL=http://magento-ce-2-3-0.local
 set MAGENTO_BACKEND_FRONTNAME=admin
 set MAGENTO_ADMIN_USER=admin
 set MAGENTO_ADMIN_PASSWORD=admin123
