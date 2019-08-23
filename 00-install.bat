b05f7710f6852eeaa4d6f80c31e41c8fffdec86e
Daniel Gheolţan <danielgheoltan@gmail.com>
2019-08-23 06:20:07 +0300

    Updates

--- 00-install.bat
+++ 00-install.bat
@@ -59,11 +59,7 @@ cd /d "!PROJECT_PATH!"
 
 set COMPOSER_AUTH={"http-basic": {"repo.magento.com": {"username": "!MAGENTO_PUBLIC_KEY!", "password": "!MAGENTO_PRIVATE_KEY!"}}}
 
-if "%1"=="" (
-    call composer create-project --repository=https://repo.magento.com/ magento/project-!MAGENTO_EDITION!-edition .
-) else (
-    call composer create-project --repository=https://repo.magento.com/ magento/project-!MAGENTO_EDITION!-edition=%1 .
-)
+call composer create-project --repository=https://repo.magento.com/ magento/project-!MAGENTO_EDITION!-edition=!MAGENTO_VERSION! .
 
 echo !COMPOSER_AUTH! > auth.json
 
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Performs a completely automated Magento 2 installation on Windows.
 
 ## Installation
 
-Edit **config.bat** and change the variables to your needs. Then open a terminal and run:
+Edit **config.bat** and change the variables to your needs. Then open a command prompt as an administrator and run:
 
 ```
 install.bat
